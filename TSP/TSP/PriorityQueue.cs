6391df155264dc768caeeb19ee8d16c1cb76a02d
sdaltonb <sdaltonb@gmail.com>
2014-12-06 13:51:46 -0700

    Added PriorityQueue skeleton. Moved TSPSolution to its own class

---
+++ TSP/TSP/PriorityQueue.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace TSP
+{
+    class PriorityQueue
+    {
+        private TSPSolution[] solutions;
+        private uint lastIndex;
+
+
+        public PriorityQueue(uint size) 
+        {
+            solutions = new TSPSolution[size];
+            lastIndex = 0;
+        }
+
+        public void push(TSPSolution solution)
+        {
+            solutions[lastIndex] = solution;
+            percolateUp(lastIndex);
+            ++lastIndex;
+        }
+
+        public TSPSolution pop()
+        {
+            TSPSolution temp = solutions[0];
+            solutions[0] = solutions[lastIndex - 1];
+
+            percolateDown(0);
+            --lastIndex;
+
+            return temp;
+        }
+
+        public TSPSolution peek()
+        {
+            return solutions[0];
+        }
+
+        public uint getSize()
+        {
+            return lastIndex;
+        }
+
+        public void clear() 
+        {
+            lastIndex = 0;
+        }
+
+        private void percolateUp(uint index)
+        {
+            if (index == 0)
+            {
+                return;
+            }
+
+            if (solutions[(int)index].costOfRoute() < solutions[(int)index / 2].costOfRoute())
+            {
+                percolateUp(index / 2);
+            }
+        }
+
+        private void percolateDown(uint siftingIndex)
+        {
+        }
+    }
+}
--- TSP/TSP/ProblemAndSolver.cs
+++ TSP/TSP/ProblemAndSolver.cs
@@ -9,51 +9,6 @@ namespace TSP
 
     class ProblemAndSolver
     {
-
-        private class TSPSolution
-        {
-            /// <summary>
-            /// we use the representation [cityB,cityA,cityC] 
-            /// to mean that cityB is the first city in the solution, cityA is the second, cityC is the third 
-            /// and the edge from cityC to cityB is the final edge in the path.  
-            /// You are, of course, free to use a different representation if it would be more convenient or efficient 
-            /// for your node data structure and search algorithm. 
-            /// </summary>
-            public ArrayList
-                Route;
-
-            public TSPSolution(ArrayList iroute)
-            {
-                Route = new ArrayList(iroute);
-            }
-
-
-            /// <summary>
-            /// Compute the cost of the current route.  
-            /// Note: This does not check that the route is complete.
-            /// It assumes that the route passes from the last city back to the first city. 
-            /// </summary>
-            /// <returns></returns>
-            public double costOfRoute()
-            {
-                // go through each edge in the route and add up the cost. 
-                int x;
-                City here;
-                double cost = 0D;
-
-                for (x = 0; x < Route.Count - 1; x++)
-                {
-                    here = Route[x] as City;
-                    cost += here.costToGetTo(Route[x + 1] as City);
-                }
-
-                // go from the last city to the first. 
-                here = Route[Route.Count - 1] as City;
-                cost += here.costToGetTo(Route[0] as City);
-                return cost;
-            }
-        }
-
         #region Private members 
 
         /// <summary>
